6f56c952c05b08947e1630cc7caf0eef27c9ef12
eonduck2 <whdtnxd@gmail.com>
2024-06-11 11:18:56 +0900

    update file 'work-1.js'

--- work-1.js
+++ work-1.js
@@ -1,15 +1,31 @@
-const formatHTML = () => {
+const formatHTML = (language) => {
   const htmlData = {
-    title: `이종수`,
-    h1: `<h1></h1>`,
-    p: `<p></p>`,
+    titleOne: `이종수`,
+    titleTwo: `Lee`,
+    h1: `ㅎㅇ`,
+    p: `ㅎㅇㅎㅎㅇ`,
+  };
+
+  const lang = () => {
+    if (language === `KOR`) {
+      return htmlData.titleOne;
+    } else if (language === `ENG`) {
+      return htmlData.titleTwo;
+    }
   };
 
   let html = `
              <html>
                 <head>
-                    <title></title>
+                    <title>${lang()}</title>
                 </head>
+                <body>
+                    <h1>${htmlData.h1}</h1>
+                    <p>${htmlData.p}</p>
+                </body>
              </html>   
             `;
+  return html;
 };
+
+console.log(formatHTML(`KOR`));
