3e8a2b1b391f6427678924d23476776af737f15e
eonduck2 <whdtnxd@gmail.com>
2024-06-11 10:55:36 +0900

    proxy

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const a = () => {
+const a = (name, age, job) => {
   return {
-    name: "",
-    age: 0,
-    job: "",
+    name: `이종수` ?? name,
+    age: 28 ?? age,
+    job: `요리사` ?? job,
   };
 };
 const b = a();
 
-console.log(b);
+const proxiedA = new Proxy(a(), {
+  get(target, prop, receiver) {
+    return Reflect.get(...arguments);
+  },
+});
+
+console.log(proxiedA);
