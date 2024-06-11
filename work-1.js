f49240efdcb311e3c476fd0426210d0f44eab7a0
eonduck2 <whdtnxd@gmail.com>
2024-06-11 11:01:01 +0900

    html template

---
+++ work-1.js
@@ -0,0 +1,15 @@
+const formatHTML = () => {
+  const htmlData = {
+    title: `이종수`,
+    h1: `<h1></h1>`,
+    p: `<p></p>`,
+  };
+
+  let html = `
+             <html>
+                <head>
+                    <title></title>
+                </head>
+             </html>   
+            `;
+};
